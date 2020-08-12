28b52c1bc0530bc2a9d752fbf3a3bfb65b1a6804
Scott Jehl <scottjehl@gmail.com>
2020-08-12 12:35:34 -0400

    gulp demo

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
 const { src, dest } = require('gulp');
 
 function copy() {
-   return src('node_modules/fg-factory/src/*.js')
+   return src([
+       'node_modules/fg-factory/src/*.js', 
+    'node_modules/wicg-inert/dist/inert.js', 
+        'node_modules/document-register-element/build/document-register-element.js'])
     .pipe(dest('demo/lib/'));
 }
+
+// var qunit = require('gulp-qunit');
+ 
+// gulp.task('test', function() {
+//     return gulp.src('./test/index.html')
+//         .pipe(qunit());
+// });
 
 exports.default = copy;
